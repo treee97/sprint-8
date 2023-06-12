6ba30fdbca11614c5ec14fc499048713b73f04d8
Luiggi <luduve97@gmail.com>
2023-06-12 18:48:03 +0200

    silly axios test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import { useState } from "react";
-
+import axios from "axios";
 import "./App.css";
 
 function App() {
-	return <></>;
+  const [planet, setPlanet] = useState("");
+  const getData = () => {
+    axios("https://swapi.dev/api/planets/1/")
+      .then((res) => setPlanet(res.data.name))
+      .catch((err) => console.error(err));
+  };
+  return (
+    <div>
+      <button onClick={getData}>Fetch Data with Axios</button>
+      {planet && <p>{planet}</p>}
+    </div>
+  );
 }
 
 export default App;
