aca32b305978b04822f7c46e10e5d0b0bfe01ed8
Luiggi <luduve97@gmail.com>
2023-06-22 12:55:07 +0200

    to be fixed

--- src/pages/Starships/Starships.tsx
+++ src/pages/Starships/Starships.tsx
@@ -5,53 +5,71 @@ import { shipType } from "../../types/types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 const Starships = () => {
-  const [ships, setShips] = useState<shipType[]>([]);
-  const [pages, setPages] = useState<number>(1);
-  const [hasMore, setHasMore] = useState<boolean>(true);
+	const [ships, setShips] = useState<shipType[]>([]);
+	const [pages, setPages] = useState<number>(1);
+	const [hasMore, setHasMore] = useState<boolean>(true);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  useEffect(() => {
-    const getShips = async () => {
-      const getShipData = await starshipCall(pages);
-      //https://www.youtube.com/watch?v=gJ5MnBVl1ZM
-      // Infinite Scroll With API Call In React JS using react-infinite-scroll-component -NPM
-      setShips(getShipData);
-      setHasMore(getShipData.length > 0);
-    };
-    getShips();
-  }, []);
+	useEffect(() => {
+		const getShips = async () => {
+			const getShipsData = await starshipCall(pages);
+			// const result= getShipsData.results
+			// console.log("NEXTT", next);
 
-  const getMoreShips = async () => {
-    const newPage = pages + 1;
-    const additionalShips = await starshipCall(newPage);
+			//https://www.youtube.com/watch?v=gJ5MnBVl1ZM
+			// Infinite Scroll With API Call In React JS using react-infinite-scroll-component -NPM
+			setShips(getShipsData);
+			setHasMore(getShipsData.length > 0);
+			// if (getShipData.length === 0) {
+			// 	setHasMore(false);
+			// }
+		};
+		getShips();
+	}, []);
 
-    if (additionalShips.length === 0) {
-      setHasMore(false);
-    } else {
-      setShips((prevShips) => [...prevShips, ...additionalShips]);
-      setPages(newPage);
-    }
-  };
+	const getMoreShips = async () => {
+		const newPage = pages + 1;
+		const additionalShips = await starshipCall(newPage);
+		const results = additionalShips.results;
+		const next = additionalShips.next;
 
-  return (
-    <InfiniteScroll
-      dataLength={ships.length} //This is important field to render the next data
-      next={getMoreShips}
-      hasMore={hasMore}
-      loader={<h4>Loading more ships...</h4>}
-      endMessage={
-        <p style={{ textAlign: "center" }}>
-          <b>Yay! You have seen it all</b>
-        </p>
-      }
-    >
-      <div>
-        <h1>starships</h1>
-        {ships.map((ship, index) => (
-          <Starship key={index} ship={ship} />
-        ))}
-      </div>
-    </InfiniteScroll>
-  );
+		if (next !== null) {
+			setPages(newPage);
+		}
+
+		console.log(pages);
+
+		console.log(additionalShips);
+
+		if (results.length === 0) {
+			setHasMore(false);
+			setIsLoading(false);
+		} else {
+			setIsLoading(true);
+			setShips((prevShips) => [...prevShips, ...additionalShips]);
+		}
+	};
+
+	return (
+		<InfiniteScroll
+			dataLength={ships.length} //This is important field to render the next data
+			next={getMoreShips}
+			hasMore={hasMore}
+			loader={isLoading && <h4>Loading more ships...</h4>}
+			endMessage={
+				<p style={{ textAlign: "center" }}>
+					<b>Yay! You have seen it all</b>
+				</p>
+			}
+		>
+			<div>
+				<h1>starships</h1>
+				{ships.map((ship, index) => (
+					<Starship key={index} ship={ship} />
+				))}
+			</div>
+		</InfiniteScroll>
+	);
 };
 
 export default Starships;
